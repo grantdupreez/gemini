327e793593d085bc168c6c6a617b72b49dee06fa
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2025-03-10 17:22:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-from langchain_community.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+import streamlit as st, os, time
+from google import genai
+from google.genai import types
+from pypdf import PdfReader, PdfWriter, PdfMerger
 import hmac
 
 
@@ -49,89 +43,256 @@ if not check_password():
     st.stop()
 
 
-def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
 
-
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
-
-
-def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
-
-
-def get_conversational_chain():
-
-    prompt_template = """
+def setup_page():
+    st.set_page_config(
+        page_title="Talk to Gemini",
+        layout="centered"
+    )
     
-    Context:\n {context}?\n
-    Question: \n{question}\n
+    st.header("Chatbot using Gemini 2.0 Flash!" )
 
-    Answer:
-    """
-
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
-                             temperature=0.3)
-
-    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
-
-    return chain
-
-
-
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    st.sidebar.header("Options", divider='rainbow')
     
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-
-    docs = new_db.similarity_search(user_question)
-
-    chain = get_conversational_chain()
+    hide_menu_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
 
     
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
+def get_choice():
+    choice = st.sidebar.radio("Choose:", ["Converse with Gemini 2.0",
+                                          "Chat with PDFs",
+                                          "Chat with an image",
+                                          "Chat with audio",
+                                          "Chat with video"],)
+    return choice
 
-    print(response)
-    st.write("Reply: ", response["output_text"])
+ 
+def get_clear():
+    clear_button=st.sidebar.button("Start new session", key="clear")
+    return clear_button
 
-
-
-
+     
 def main():
-    st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    choice = get_choice()
+    
+    if choice == "Converse with Gemini 2.0":
+        st.subheader("Ask Gemini")
+        clear = get_clear()
+        if clear:
+            if 'message' in st.session_state:
+                del st.session_state['message']
+    
+        if 'message' not in st.session_state:
+            st.session_state.message = " "
+        
+        if clear not in st.session_state:
+            chat = client.chats.create(model=MODEL_ID, config=types.GenerateContentConfig(
+                system_instruction="You are a helpful assistant. Your answers need to brief and concise.",))
+            prompt = st.chat_input("Enter your question here")
+            if prompt:
+                with st.chat_message("user"):
+                    st.write(prompt)
+        
+                st.session_state.message += prompt
+                with st.chat_message(
+                    "model", avatar="🧞‍♀️",
+                ):
+                    response = chat.send_message(st.session_state.message)
+                    st.markdown(response.text) 
+                    st.sidebar.markdown(response.usage_metadata)
+                st.session_state.message += response.text
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
+                        
+    elif choice == "Chat with PDFs":
+        st.subheader("Chat with your PDF files")
+        clear = get_clear()
+        if clear:
+            if 'message' in st.session_state:
+                del st.session_state['message']
+    
+        if 'message' not in st.session_state:
+            st.session_state.message = " "
+        
+        if clear not in st.session_state:
+        
+            uploaded_files2 = st.file_uploader("Choose 1 or more files",  type=['pdf'], accept_multiple_files=True)
+               
+            if uploaded_files2:
+                merger = PdfWriter()
+                for file in uploaded_files2:
+                        merger.append(file)
+    
+                fullfile = "merged_all_files.pdf"
+                merger.write(fullfile)
+                merger.close()
 
-    if user_question:
-        user_input(user_question)
+                file_upload = client.files.upload(file=fullfile) 
+                chat2b = client.chats.create(model=MODEL_ID,
+                    history=[
+                        types.Content(
+                            role="user",
+                            parts=[
+    
+                                    types.Part.from_uri(
+                                        file_uri=file_upload.uri,
+                                        mime_type=file_upload.mime_type),
+                                    ]
+                            ),
+                        ]
+                        )
+                prompt2b = st.chat_input("Enter your question here")
+                if prompt2b:
+                    with st.chat_message("user"):
+                        st.write(prompt2b)
+            
+                    st.session_state.message += prompt2b
+                    with st.chat_message(
+                        "model", avatar="🧞‍♀️",
+                    ):
+                        response2b = chat2b.send_message(st.session_state.message)
+                        st.markdown(response2b.text)
+                        st.sidebar.markdown(response2b.usage_metadata)
+                    st.session_state.message += response2b.text
+            
+    elif choice == "Chat with an image":
+        st.subheader("Chat with your image file")
+        clear = get_clear()
+        if clear:
+            if 'message' in st.session_state:
+                del st.session_state['message']
+    
+        if 'message' not in st.session_state:
+            st.session_state.message = " "
+        
+        if clear not in st.session_state:
+            uploaded_files2 = st.file_uploader("Choose your PNG or JPEG file",  type=['png','jpg'], accept_multiple_files=False)
+            if uploaded_files2:
+                file_name2=uploaded_files2.name
+                file_upload = client.files.upload(file=file_name2)
+                chat3 = client.chats.create(model=MODEL_ID,
+                    history=[
+                        types.Content(
+                            role="user",
+                            parts=[
+    
+                                    types.Part.from_uri(
+                                        file_uri=file_upload.uri,
+                                        mime_type=file_upload.mime_type),
+                                    ]
+                            ),
+                        ]
+                        )
+                prompt3 = st.chat_input("Enter your question here")
+                if prompt3:
+                    with st.chat_message("user"):
+                        st.write(prompt3)
+            
+                    st.session_state.message += prompt3
+                    with st.chat_message(
+                        "model", avatar="🧞‍♀️",
+                    ):
+                        response3 = chat3.send_message(st.session_state.message)
+                        st.markdown(response3.text)
+                    st.session_state.message += response3.text
+                
+    elif choice == "Chat with audio":
+        st.subheader("Chat with your audio file")
+        clear = get_clear()
+        if clear:
+            if 'message' in st.session_state:
+                del st.session_state['message']
+    
+        if 'message' not in st.session_state:
+            st.session_state.message = " "
+        
+        if clear not in st.session_state:
+            uploaded_files3 = st.file_uploader("Choose your mp3 or wav file",  type=['mp3','wav'], accept_multiple_files=False)
+            if uploaded_files3:
+                file_name3=uploaded_files3.name
+                file_upload = client.files.upload(file=file_name3)
+                chat4 = client.chats.create(model=MODEL_ID,
+                    history=[
+                        types.Content(
+                            role="user",
+                            parts=[
+    
+                                    types.Part.from_uri(
+                                        file_uri=file_upload.uri,
+                                        mime_type=file_upload.mime_type),
+                                    ]
+                            ),
+                        ]
+                        )
+                prompt5 = st.chat_input("Enter your question here")
+                if prompt5:
+                    with st.chat_message("user"):
+                        st.write(prompt5)
+            
+                    st.session_state.message += prompt5
+                    with st.chat_message(
+                        "model", avatar="🧞‍♀️",
+                    ):
+                        response4 = chat4.send_message(st.session_state.message)
+                        st.markdown(response4.text)
+                    st.session_state.message += response4.text
 
-    with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
-
-
-
-if __name__ == "__main__":
+    elif choice == "Chat with video":
+        st.subheader("Chat with your video file")
+        clear = get_clear()
+        if clear:
+            if 'message' in st.session_state:
+                del st.session_state['message']
+    
+        if 'message' not in st.session_state:
+            st.session_state.message = " "
+        
+        if clear not in st.session_state:
+            uploaded_files4 = st.file_uploader("Choose your mp4 or mov file",  type=['mp4','mov'], accept_multiple_files=False)
+            
+            if uploaded_files4:
+                file_name4=uploaded_files4.name
+                video_file = client.files.upload(file=file_name4)
+                while video_file.state == "PROCESSING":
+                    time.sleep(10)
+                    video_file = client.files.get(name=video_file.name)
+                
+                if video_file.state == "FAILED":
+                  raise ValueError(video_file.state)
+                
+                chat5 = client.chats.create(model=MODEL_ID,
+                    history=[
+                        types.Content(
+                            role="user",
+                            parts=[
+    
+                                    types.Part.from_uri(
+                                        file_uri=video_file.uri,
+                                        mime_type=video_file.mime_type),
+                                    ]
+                            ),
+                        ]
+                        )
+                prompt4 = st.chat_input("Enter your question here")
+                if prompt4:
+                    with st.chat_message("user"):
+                        st.write(prompt4)
+            
+                    st.session_state.message += prompt4
+                    with st.chat_message(
+                        "model", avatar="🧞‍♀️",
+                    ):
+                        response5 = chat5.send_message(st.session_state.message)
+                        st.markdown(response5.text)
+                    st.session_state.message += response5.text
+                    
+                
+if __name__ == '__main__':
+    setup_page()
+    api_key = st.secrets['auth_key']
+    client = genai.Client(api_key=api_key)
+    MODEL_ID = "gemini-2.0-flash-001"
     main()
-
