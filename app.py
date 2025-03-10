c6fbcec5796bc6522d52e1f1ba802003c02cec6b
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2025-03-10 16:39:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
-from langchain.chains.question_answering import load_qa_chain
+from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain_community.vectorstores import FAISS
-import PyPDF2
 import os
-import io
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
 import hmac
 
 
@@ -50,75 +49,89 @@ if not check_password():
     st.stop()
 
 
-st.title("Chat Your PDFs")  # Updated title
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
 
-# Retrieve API key from environment variable
-google_api_key = st.secrets['auth_key']
 
-# Check if the API key is available
-if google_api_key is None:
-    st.warning("API key not found. Please set the google_api_key environment variable.")
-    st.stop()
 
-# Check if the API key is available
-if google_api_key is None:
-    st.warning("API key not found. Please set the google_api_key environment variable.")
-    st.stop()
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
 
-# File Upload with user-defined name
-uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-if uploaded_file is not None:
-    st.text("PDF File Uploaded Successfully!")
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
 
-    # PDF Processing (using PyPDF2 directly)
-    pdf_data = uploaded_file.read()
-    pdf_reader = PyPDF2.PdfReader(io.BytesIO(pdf_data))
-    pdf_pages = pdf_reader.pages
 
-    # Create Context
-    context = "\n\n".join(page.extract_text() for page in pdf_pages)
+def get_conversational_chain():
 
-    # Split Texts
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=200)
-    texts = text_splitter.split_text(context)
+    prompt_template = """
+    
+    Context:\n {context}?\n
+    Question: \n{question}\n
 
-    # Chroma Embeddings
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vector_index = Chroma.from_texts(texts, embeddings).as_retriever()
+    Answer:
+    """
 
-    # Get User Question
-    user_question = st.text_input("Ask a Question:")
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
 
-    if st.button("Get Answer"):
-        if user_question:
-            # Get Relevant Documents
-            docs = vector_index.get_relevant_documents(user_question)
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
-            # Define Prompt Template
-            prompt_template = """
-            Answer the question as detailed as possible from the provided context,
-            make sure to provide all the details, if the answer is not in
-            provided context just say, "answer is not available in the context",
-            don't provide the wrong answer\n\n
-            Context:\n {context}?\n
-            Question: \n{question}\n
-            Answer:
-            """
+    return chain
 
-            # Create Prompt
-            prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
 
-            # Load QA Chain
-            model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3, api_key=google_api_key)
-            chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
 
-            # Get Response
-            response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
 
-            # Display Answer
-            st.subheader("Answer:")
-            st.write(response['output_text'])
+    docs = new_db.similarity_search(user_question)
 
-        else:
-            st.warning("Please enter a question.")
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
+
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+
+    if user_question:
+        user_input(user_question)
+
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
+
